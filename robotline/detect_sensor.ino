7e66d4ccdb7b8be885677d175c884fcebe361ed5
Ditokasiditz <kasidit.samad@gmail.com>
2022-11-07 17:52:46 +0700

    add_needcode

---
+++ robotline/detect_sensor.ino
@@ -0,0 +1,13 @@
+void Detect_ss()
+{
+  Serial.print("Left 2 :");
+  Serial.print(analogRead(2))
+  Serial.print("Left 3 :");
+  Serial.print(analogRead(3))
+  Serial.print("Mid 4 :");
+  Serial.print(analogRead(4))
+  Serial.print("Right 5 :");
+  Serial.print(analogRead(5))
+  Serial.print("Right 6 :");
+  Serial.println(analogRead(6))
+}
---
+++ robotline/motor.ino
@@ -0,0 +1,42 @@
+void Motor(int spl,int spr)
+{
+  pinMode(10,OUTPUT);//pwmL
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+ 
+  pinMode(11,OUTPUT);//pwmR
+  pinMode(9,OUTPUT);
+  pinMode(12,OUTPUT);
+  //spl > 100, spr > 100  limit motor speed for 100
+  if (spl>100)
+  spl=100;
+  else if (spr>100)
+  spr=100;
+  //Motor Left
+  if(spl>0)
+    analogWrite(10,(spl*255)/100);
+    digitalWrite(7,HIGH);
+    digitalWrite(8,LOW);
+  else if (spl<0)
+    analogWrite(10,(-spl*255)/100);
+    digitalWrite(7,LOW);
+    digitalWrite(8,HIGH);
+  else
+    analogWrite(10,(spl*255)/100);
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW);
+
+  //Motor Right
+  if(spr>0)
+    analogWrite(11,(spr*255)/100);
+    digitalWrite(9,HIGH);
+    digitalWrite(12,LOW);
+  else if (spl<0)
+    analogWrite(11,(-spr*255)/100);
+    digitalWrite(9,LOW);
+    digitalWrite(12,HIGH);
+  else
+    analogWrite(11,(spr*255)/100);
+    digitalWrite(9,LOW);
+    digitalWrite(12,LOW);
+}
