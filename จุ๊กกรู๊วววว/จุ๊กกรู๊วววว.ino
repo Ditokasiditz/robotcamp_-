ee5de494dcf193f72752c11e8bd06ae86613776d
Ditokasiditz <kasidit.samad@gmail.com>
2022-11-09 20:59:39 +0700

    klj

--- จุ๊กกรู๊วววว/จุ๊กกรู๊วววว.ino
+++ จุ๊กกรู๊วววว/จุ๊กกรู๊วววว.ino
@@ -14,20 +14,12 @@
 //motor speed
 int spl=100,spr=100;
 
-//PID calculate
-float Kp=30,Ki=0,Kd=15;
-float error=0, P=0, I=0, D=0, PID_value=0;
-float previous_error=0,previous_I=0;
-
-
 void setup() {
   Serial.begin(9600);
 }
   
 void loop() 
 {
-  read_sensor_values();
-  Calculate_PID();
   run_case(spl,spr);
 }
 
@@ -59,7 +51,6 @@ void run_case(int spl, int spr)
   Motor(0,0);
   delay(10);
 }
-
 
 void Motor(int spl,int spr)
 {
@@ -117,76 +108,5 @@ void Motor(int spl,int spr)
     digitalWrite(RM2,LOW);
   }
 }
-
-
-void Calculate_PID()
-{
-  // คำนวณค่าความคลาดเคลื่อนในการเดินตามเส้น
-  P= error;
-  I= I + previous_I;
-  D= error-previous_error;
-  PID_value = (Kp*P) + (Ki*I) + (Kd*D);
-  previous_I=I;
-  previous_error=error;
-  
-  // Check PID
-  Serial.print(PID_value);
-  Serial.print(' ');
-  Serial.print(previous_I);
-  Serial.print(' ');
-  Serial.print(previous_error);
-  Serial.print(' ');
-  Serial.println();
-  delay(500); 
-}
-
-
-
-
-void read_sensor_values()
-{
-
-  pinMode(LS,INPUT);
-  pinMode(MS,INPUT);
-  pinMode(RS,INPUT);
-  //define senssor
-  int Left,Mid,Right;
-  Left=analogRead(LS);
-  Mid=analogRead(MS);
-  Right=analogRead(RS);
-  
-  //check senssor
-      Serial.println(analogRead(LS));
-      Serial.println(analogRead(MS));
-      Serial.println(analogRead(RS));
-      delay(200);
-
-
-  //ในการเคลื่อนที่ เมื่อเซนเซอร์เมื่อ เซนเซอร์ซ้ายขวาจับได้ว่า พบพื้นที่สีดำ เป็น 0 ในค่า digital และ < 350 ใน analog
-  // พื้นที่ขาว = 0 (in Digital)   or   (<50 in Analog)
-  // พื้นที่ดำ  = 1 (in Digital)   or   (>800 in Analog)
-  //Black line
-  if( (Left <250 ) && (Mid < 250) && (Right >=600 )) // 0 0 1 *****************
-  {
-    error= 2; // เพียงบอกว่า หุ่นเอียงไปจากเเนวเส้นไปทางซ้าย ในระดับ 2 
-  }
-  else if( (Left<250) && (Mid>=600) && (Right>=600)) // 0 1 1 *****************
-  {
-    error= 1; // เพียงบอกว่า หุ่นเอียงไปจากเเนวเส้นไปทางซ้าย ในระดับ 1
-  }
-  else if( (Left<250 ) && (Mid>600) && (Right<250)) // 0 1 0 *****************
-  {
-    error= 0; // เพียงบอกว่า อยู่ในแนวเส้นตรง ในระดับ 0
-  }
-  else if( (Left>600) && (Mid>600) && (Right<250)) // 1 1 0 ******************
-  {
-    error= -1; // เพียงบอกว่า หุ่นเอียงไปจากเเนวเส้นไปทางขวา ในระดับ -1
-  }
-  else if( (Left>600) && (Mid<250) && (Right<250)) // 1 0 0  ***************
-  {
-    error= -2; // เพียงบอกว่า หุ่นเอียงไปจากเเนวเส้นไปทางขวา ในระดับ -2
-  }
-}
-  
 
 
